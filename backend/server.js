e0442d60ee0598ddb0788e943799a3d0c366ee19
jor2611 <khachatryanj007@gmail.com>
2019-09-17 14:37:35 +0400

    Environment setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+require('dotenv').config()
+
+const port = process.env.PORT;
+
+http.listen(port,()=>{
+    console.log(`Server running on port ${port}...`);
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
